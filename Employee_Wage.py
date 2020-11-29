b47844e0953b6a128af734897dcdec5857be840d
gautamb97 <www.gautam971997@gmail.com>
2020-11-29 17:59:17 +0530

    [Gautam]Added:employee_pressence

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -1,1 +1,15 @@
-print("Welcome to Employee Wage Computation")
+import random
+
+
+class EmployeeWage:
+    print("Welcome to Employee Wage Computation")
+    def checkAttendance(self):
+        if(random.randint(0,1)) == 1:
+            print("Employee is Present")
+        else:
+            print("Employee is Absent")
+
+if __name__=="__main__":
+
+    Employee = EmployeeWage()
+    (Employee.checkAttendance())
