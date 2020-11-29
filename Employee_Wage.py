e8a87d44aebaf9d5f15622d04fda08d94454bf00
gautamb97 <www.gautam971997@gmail.com>
2020-11-29 20:11:53 +0530

    [Gautam]Added:calculated_employee_daily_wage

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -3,13 +3,24 @@ import random
 
 class EmployeeWage:
     print("Welcome to Employee Wage Computation")
+    employeeCheck = (random.randint(0, 1))
     def checkAttendance(self):
-        if(random.randint(0,1)) == 1:
+        if EmployeeWage.employeeCheck == 1:
             print("Employee is Present")
         else:
             print("Employee is Absent")
+
+    def dailyWage(self):
+        EMP_RATE_PER_HOUR = 20
+        if EmployeeWage.employeeCheck == 1:
+            empHrs = 8
+        else:
+            empHrs = 0
+        empWage = empHrs * EMP_RATE_PER_HOUR
+        print("Employee daily Wage is : ",empWage)
 
 if __name__=="__main__":
 
     Employee = EmployeeWage()
     (Employee.checkAttendance())
+    (Employee.dailyWage())
