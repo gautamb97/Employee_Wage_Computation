c20517fee70b644f9496e855dd9a80657735d837
gautamb97 <www.gautam971997@gmail.com>
2020-11-30 09:53:19 +0530

    [Gautam]Added:total_wage_for_a_month

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -4,6 +4,8 @@ import random
 class EmployeeWage:
     print("Welcome to Employee Wage Computation")
     employeeCheck = (random.randint(0, 1))
+    EMP_RATE_PER_HOUR = 20
+    totalWage = 0
     def checkAttendance(self):
         if EmployeeWage.employeeCheck == 1:
             print("Employee is Present")
@@ -11,19 +13,22 @@ class EmployeeWage:
             print("Employee is Absent")
 
     def dailyWage(self):
-        EMP_RATE_PER_HOUR = 20
-        if EmployeeWage.employeeCheck == 1:
-            partTime = (random.randint(0,1))
-            if partTime == 0:
-                empHrs = 4
-                print("Employee is part time worker")
+        for days in range(21):
+            if EmployeeWage.employeeCheck == 1:
+
+                    partTime = (random.randint(0,1))
+                    if partTime == 0:
+                        empHrs = 4
+                    else:
+                        empHrs = 8
+
             else:
-                empHrs = 8
-                print("Employee is full time worker")
-        else:
-            empHrs = 0
-        empWage = empHrs * EMP_RATE_PER_HOUR
-        print("Employee daily Wage is : ",empWage)
+                empHrs = 0
+            empWage = empHrs * EmployeeWage.EMP_RATE_PER_HOUR
+            EmployeeWage.totalWage += empWage
+
+        print("Total wage is: ", EmployeeWage.totalWage)
+
 
 if __name__=="__main__":
 
